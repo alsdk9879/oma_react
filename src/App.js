9454e71e62ad8932625099c699445eb7a9909aff
Mina <alsdk9879@infinitum.co.kr>
2024-04-16 16:29:42 +0900

    UserList.js > useEffect 진행중

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
     email: '',
   });
   const { username, email } = inputs;
+
   const onChange = (e) => {
     const { name, value } = e.target;
     SetInputs({
@@ -30,27 +31,49 @@ function App() {
     });
   };
 
-  const users = [
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
       email: 'public.velopert@gmail.com',
+      active: true,
     },
     {
       id: 2,
       username: 'tester',
       email: 'tester@example.com',
+      active: false,
     },
     {
       id: 3,
       username: 'liz',
       email: 'liz@example.com',
+      active: false,
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    SetInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)));
   };
 
   return (
@@ -62,8 +85,8 @@ function App() {
       <Counter />
       <div className='gray-box'>graybox</div>
       <InputSample />
-      <CreateUser />
-      <UserList users={users} />
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </Wrapper>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // 한페이지에 여러개의 컴포넌트가 있어도 무방
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log('컴포넌트 나타남');
+    return () => {
+      console.log('컴포넌트 사라짐');
+    };
+  }, []);
   return (
     <>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b style={{ cursor: 'pointer', color: user.active ? 'green' : 'black' }} onClick={() => onToggle(user.id)}>
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button className='sm' onClick={() => onRemove(user.id)}>
+        삭제
+      </button>
     </>
   );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
       ))}
     </div>
   );
